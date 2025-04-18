c68338f28046a1acc0df8a97d7951dfa4d662de5
Willian-fg <willflorgasp@gmail.com>
2025-04-18 09:07:40 -0300

    update init.py

--- init.py
+++ init.py
@@ -2,4 +2,8 @@ import flat as ft
 
 class JanelaPrincipal:
   def __init__(self, page: ft.Page):
+    self.page = page
+    self.page.bgcolor = ft.colors.WHITE
+    self.page.window_width = 350
+    
     
