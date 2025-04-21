3f5faa4385e97c5a8874e97cbf48ce25244f0f33
willian-fg <willflorgasp@gmail.com>
2025-04-21 17:01:58 -0300

    Reestruturado

--- init.py
+++ init.py
@@ -8,16 +8,67 @@ from flet import(
       Page,
       Row,
       Tab,
+      Tabs,
       Text,
       TextField,
-      #userControl,
       colors,
       icons,
 )
 
+#Classes
+
+class Task():
+      def __init__(self, task_name, task_status_change, task_delete):
+            super().__init__()
+            self.completed = False
+            self.task_name = task_name
+            self.task_status_change = task_delete
+            
+class Silly():
+      def build(self):
+            self.new_task = TextField(
+                  hint_text = 'Escreva a Tarefa',
+                  expand = True,
+                  on_submit = self.add_clicked
+
+            
+            )
+            self.task = Column()
+
+            self.filter = Tabs(
+                  selected_index = 0,
+                  on_change = self.tabs_changes,
+                  tabs = [Tab(text = 'Todas'), Tab(text = 'Ativas'), Tab(text = 'Completas')]
+            )
+
+            return Column(
+                  width = 600,
+                  controls = [
+                        Row([Text(value='Tarefas',
+                                  style='headlineMedium')], alignment = 'center'),
+                        Row()
+
+                  ]
+                        
+            )
+
+      
+      def add_clicked(self):
+            pass
+            
 
 
+#Funçâo Principal
 def main(page: Page):
-      pass
+      page.title = "Silly"
+      page.horizontal_alignment = 'center'
+      page.scroll = 'adaptative'
+      page.update()
+
+      app = Silly() #Instancia da classe principal
+
+      page.add(app) #Adição da Calsse Primcipal
+
+
 
 flet.app(target=main)
